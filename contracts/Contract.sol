e7d2535eced3c8af022e16b15da4fab1c298df80
Matthew Drumheller <MattDrummy@gmail.com>
2017-09-19 14:12:45 -0600

    create test-truffle-production

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -17,7 +17,7 @@ contract CrowdFunding {
     mapping (uint => Campaign) campaigns;
     function newCampaign(address beneficiary, uint goal, uint deadline) returns (uint campaignID) {
         campaignID = numCampaigns++;
-        Campaign c = campaigns[campaignID]; 
+        Campaign c = campaigns[campaignID];
         c.beneficiary = beneficiary;
         c.fundingGoal = goal;
         c.deadline = block.number + deadline;
