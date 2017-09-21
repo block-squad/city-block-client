b6fefea901f3e7dfad8046d23418a764404853d8
Erin Shaben <eshaben@icloud.com>
2017-09-21 07:57:04 -0600

    restored footer to previous stylings, added contribution functionality to projects

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,13 @@
   <div id="app">
     <mainNav :isSignedIn="isSignedIn"></mainNav>
     <router-view></router-view>
-    <bottom :web3Provider="web3Provider" :web3="web3"></bottom>
+    <bottom></bottom>
   </div>
 </template>
 
 <script>
 import MainNav from './components/MainNav'
 import Bottom from './components/Bottom'
-import Web3 from 'web3'
-
-
 export default {
   name: 'app',
   computed: {
@@ -24,29 +21,6 @@ export default {
       }
     }
   },
-  data(){
-  return {
-    web3Provider: '',
-    web3: ''
-  }
-},
-methods: {
-  method1: function() {
-    if (typeof this.web3 !== 'undefined') {
-      this.web3Provider = web3.currentProvider;
-      this.web3 = new Web3(web3.currentProvider)
-    } else {
-      console.log('Injected web3 Not Found!!!')
-      this.web3Provider = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-      this.web3 = new Web3(this.web3Provider)
-    }
-    console.log(this.web3);
-    console.log(this.web3Provider);
-  }
-},
-beforeMount(){
-  this.method1();
-},
   components: {
     MainNav,
     Bottom
--- src/components/Bottom.vue
+++ src/components/Bottom.vue
@@ -4,16 +4,13 @@
       <div class="container">
         <div class="content has-text-centered">
           <p>
-            <strong>cityBlock</strong> by <a href="https://github.com/block-squad/">The Block Squad </a>. The source code is licensed
-            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-            is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+            <img height="30" width="100" src="../assets/logo.jpg" alt="City Block"> by <a href="https://github.com/block-squad/">The Block Squad</a>.
           </p>
-          <p>
-            <a class="icon" href="https://github.com/jgthms/bulma">
-              <!-- <i class="fa fa-github"></i> -->
-            </a>
-          </p>
-          <button v-on:click="method1" type="button" name="button">TEST</button>
+          <h1 class="heading">Team Members</h1>
+          <a href="#" target="_blank">Ellen Downing</a> |
+          <a href="#" target="_blank">Matt Drumheller</a> |
+          <a href="#" target="_blank">Maria Jose Mata</a> |
+          <a href="#" target="_blank">Erin Shaben</a>
         </div>
       </div>
     </footer>
@@ -21,41 +18,7 @@
 </template>
 
 <script>
-import contract from "truffle-contract"
-import CrowdFunding from '../../build/contracts/CrowdFunding.json'
 export default {
-  name: 'bottom',
-  props: ['web3Provider', 'web3'],
-  methods: {
-    method1(event){
-      event.preventDefault();
-      let provider = this.web3Provider;
-      let deployedInstance;
-      let MyContract = contract(CrowdFunding);
-      MyContract.setProvider(provider)
-      MyContract.defaults({
-        from: '0x1127f32f3bfa30b18ad837085d370ac83fc22ea7',
-      })
-
-      MyContract.deployed().then(function(instance){
-        console.log(instance);
-        deployedInstance = instance;
-        return deployedInstance.contribute(1)
-      }).then(function(result){
-        console.log("success");
-        console.log(result);
-      })
-
-      // MyContract.deployed().then(instance => {
-      //   let deployedInstance = instance
-      //   deployedInstance.contribute(1)
-      // }).then(result => {
-      //   console.log(result);
-      //   console.log("hiiii!");
-      // }).catch(err => {
-      //   console.log(err);
-      // })
-    }
-  }
+  name: 'bottom'
 }
 </script>
