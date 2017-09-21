abb3f57034b58390a7172d1b6ca80423e81f6703
EllenDown <ellenldowning@gmail.com>
2017-09-21 11:59:57 -0600

    added how-to and links to linkin pages

--- src/components/Bottom.vue
+++ src/components/Bottom.vue
@@ -7,10 +7,10 @@
             <img height="30" width="100" src="../assets/logo.jpg" alt="City Block"> by <a href="https://github.com/block-squad/">The Block Squad</a>.
           </p>
           <h1 class="heading">Team Members</h1>
-          <a href="#" target="_blank">Ellen Downing</a> |
-          <a href="#" target="_blank">Matt Drumheller</a> |
-          <a href="#" target="_blank">Maria Jose Mata</a> |
-          <a href="#" target="_blank">Erin Shaben</a>
+          <a href="https://www.linkedin.com/in/ellen-downing/" target="_blank">Ellen Downing</a> |
+          <a href="https://www.linkedin.com/in/mattdrummy/" target="_blank">Matt Drumheller</a> |
+          <a href="https://www.linkedin.com/in/mjmata/" target="_blank">Maria Jose Mata</a> |
+          <a href="https://www.linkedin.com/in/eshaben/" target="_blank">Erin Shaben</a>
         </div>
       </div>
     </footer>
