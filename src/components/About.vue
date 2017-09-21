f83ac9cb118a3247c98cac4d03c62ad599c04c8e
EllenDown <ellenldowning@gmail.com>
2017-09-21 14:06:01 -0600

    changes to about and footer

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,22 +4,22 @@
     </div>
     <div class="howto box container">
       <h1 class="title"><strong>How To Use cityBlock: </strong></h1>
-      <ol>
+      <ul>
         <li> If you don’t already have one, you will need an <a href="https://ethereum.org">Ethereum Wallet</a> fully loaded with <a href="https://ethereum.org/ether">Ether</a>  to interact with this application. </li>
         <br>
         <li> To make it easy on yourself you should also install <a href="https://metamask.io/">MetaMask</a>, a Chrome extension that hosts your Ethereum Wallet, will allow you to create new projects or contribute to existing projects on cityBlock.  </li>
-          <br>
-        <li> Once you have MetaMask installed make sure you have the network (in the upper right corder of MetaMask) set to <strong>‘localhost 8545’.</strong></li>
+        <br>
+        <li> Once you have MetaMask installed make sure you have the network (in the upper right corner of MetaMask) set to <strong>‘localhost 8545’.</strong></li>
         <br>
         <li> Create an account on cityBlock by creating a username and password and providing the public key (aka account address) of your Wallet. </li>
         <br>
         <li> The projects hosted on cityBlock are projects that your fellow Denverites have proposed in an effort to improve the urban environment. Provided the target amount is raised via community contributions the city has agreed to initiate these projects and supply the resources necessary to complete them. </li>
         <br>
         <li> If the target amount is not met by the deadline the value of each contribution made will be returned to the Wallet of each contributor. </li>
-      </ol>
+      </ul>
       <br>
       <div class="thanks">
-        <h3> <strong>with <span className="footer_gradient_text">&hearts;</span> Block Squad</strong></h3>
+        <h3> <strong>made with <span className="footer_gradient_text">&hearts;</span> by Block Squad</strong></h3>
       </div>
     </div>
   </div>
@@ -50,6 +50,11 @@ export default {
     margin-top: 3vh;
     margin-bottom: 3vh;
     padding: 50px;
+  }
+
+  li {
+    list-style-type: disc;
+
   }
 
 </style>
--- src/components/Bottom.vue
+++ src/components/Bottom.vue
@@ -3,10 +3,8 @@
     <footer class="footer">
       <div class="container">
         <div class="content has-text-centered">
-          <p>
-            <img height="30" width="100" src="../assets/logo.jpg" alt="City Block"> by <a href="https://github.com/block-squad/">The Block Squad</a>.
-          </p>
-          <h1 class="heading">Team Members</h1>
+          <p><img height="30" width="200" src="../assets/logo.jpg" alt="City Block"></p>
+          <h3> <strong>made with <span className="footer_gradient_text">&hearts;</span> by <a href="https://github.com/block-squad/"> Block Squad</a></strong></h3>
           <a href="https://www.linkedin.com/in/ellen-downing/" target="_blank">Ellen Downing</a> |
           <a href="https://www.linkedin.com/in/mattdrummy/" target="_blank">Matt Drumheller</a> |
           <a href="https://www.linkedin.com/in/mjmata/" target="_blank">Maria Jose Mata</a> |
